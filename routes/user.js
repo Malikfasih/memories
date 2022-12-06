6071ce9bbd853f880075c35282c44c9778f99607
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2022-12-06 08:03:05 +0500

    some changes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,9 @@
-import express from "express";
-import { signin, signup } from "../controllers/user.js";
+import express from 'express';
+import { signin, signup } from '../controllers/user.js';
 
 const router = express.Router();
 
-// here signin/siginup form data will be posted in db and we can apply controller function as well i.e signin & signup
-router.post("/signin", signin);
-router.post("/signup", signup);
+router.post('/signin', signin);
+router.post('/signup', signup);
 
 export default router;
